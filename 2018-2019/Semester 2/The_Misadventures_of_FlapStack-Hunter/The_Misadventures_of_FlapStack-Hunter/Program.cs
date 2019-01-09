a0267d2b9e04ce2fab29d0574c590f278c11ef9d
Hunterkoloski <hunterkoloski@gmail.com>
2019-01-09 14:42:57 -0700

    The successful adventure of flapjack.

---
+++ 2018-2019/Semester 2/The_Misadventures_of_FlapStack-Hunter/The_Misadventures_of_FlapStack-Hunter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Misadventures_of_FlapStack_Hunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack mystack = new Stack();
+            int input;
+            do
+            {
+                Console.WriteLine("**********************");
+                Console.WriteLine("*** 1. Push     ******");
+                Console.WriteLine("*** 2. Pop      ******");
+                Console.WriteLine("*** 3. Peek     ******");
+                Console.WriteLine("*** 4. Print    ******");
+                Console.WriteLine("*** Enter 5 to exit **");
+                Console.WriteLine("**********************");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("Type a value that you would like to add");
+                    int numb = int.Parse(Console.ReadLine());
+                    mystack.Push(numb);
+                }
+
+                if (input == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine(mystack.Pop());
+                }
+
+                if (input == 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine(mystack.Peek());
+                }
+
+                if (input == 4)
+                {
+                    mystack.Print();
+                }
+
+            } while (input != 5);
+        }
+       
+    }
+}
---
+++ 2018-2019/Semester 2/The_Misadventures_of_FlapStack-Hunter/The_Misadventures_of_FlapStack-Hunter/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Misadventures_of_FlapStack_Hunter
+{
+    class Stack
+    {
+        List<int> holder = new List<int>();
+        
+        public Stack()
+        {
+
+        }
+        //push
+        public void Push(int val)
+        {
+           
+            holder.Add(val);
+
+        }
+        //pop
+        public int Pop()
+        {
+            if (holder.Count > 1)
+            {
+
+
+                int Boolin = holder[holder.Count - 1];
+                holder.RemoveAt(holder.Count - 1);
+                return Boolin;
+            }
+            else
+                return 000;
+        }
+        //peek
+        public int Peek()
+        {
+            if (holder.Count > 1)
+            {
+                int Boolin = holder[holder.Count - 1];
+                return Boolin;
+            }
+            else
+                return 000;
+        }
+        //print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count - 1; i++)
+            {
+                Console.WriteLine(holder[i]);
+            }
+
+        }
+    }
+
+
+}
+
