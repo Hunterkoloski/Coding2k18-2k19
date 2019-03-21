4d5c3b954ead2c8bf0002f809ae3b5d8dca6fc18
Hunterkoloski <hunterkoloski@gmail.com>
2019-03-21 13:08:23 -0600

    Changed the problem with algoritham practice

--- 2018-2019/Semester 2/Algorithim Practice/Algorithim Practice/Program.cs
+++ 2018-2019/Semester 2/Algorithim Practice/Algorithim Practice/Program.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
-namespace Algorithim_Practice
+namespace Algorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            int answer = 0;
+            do
             {
-                int answer = 0;
-                Console.WriteLine("1 - Problem");
+
+
+                Console.WriteLine("1 - Problem 1");
                 Console.WriteLine("2 - problem 2");
+                Console.WriteLine("3 - problem 3");
+                Console.WriteLine("4 - Leave");
+
                 answer = int.Parse(Console.ReadLine());
                 switch (answer)
                 {
@@ -23,53 +27,49 @@ namespace Algorithim_Practice
                         string text = AppDomain.CurrentDomain.BaseDirectory + @"Prob01.in.txt";
                         List<string> file = new List<string>();
                         using (StreamReader sr = new StreamReader(text))
-
                         {
                             string line;
                             while ((line = sr.ReadLine()) != null)
                             {
                                 file.Add(line);
                             }
-
                         }
-                        float ttl = 0;
-                        float subttl = 0;
-
+                        float beach = 0;
+                        float ball = 0;
                         string amount = "";
                         for (int i = 0; i < file.Count; i++)
                         {
-                            subttl = 0;
+                            beach = 0;
                             if (file[i].Contains("QUARTER"))
                             {
                                 amount = file[i].Substring(8);
-                                subttl = .25f * float.Parse(amount);
+                                beach = .25f * float.Parse(amount);
                             }
                             if (file[i].Contains("DIME"))
                             {
                                 amount = file[i].Substring(5);
-                                subttl = .10f * float.Parse(amount);
+                                beach = .10f * float.Parse(amount);
                             }
                             if (file[i].Contains("NICKEL"))
                             {
                                 amount = file[i].Substring(7);
-                                subttl = .05f * float.Parse(amount);
-
+                                beach = .05f * float.Parse(amount);
                             }
                             if (file[i].Contains("PENNY"))
                             {
                                 amount = file[i].Substring(6);
-                                subttl = .01f * float.Parse(amount);
+                                beach = .01f * float.Parse(amount);
                             }
                             if (file[i].Contains("HALFDOLLAR"))
                             {
                                 amount = file[i].Substring(11);
-                                subttl = .50f * float.Parse(amount);
+                                beach = .50f * float.Parse(amount);
                             }
+                            ball = ball + beach;
 
-                            ttl = ttl + subttl;
+
                         }
-
-                        Console.WriteLine("$" + ttl);
+                        Console.WriteLine("$" + ball);
                         Console.ReadLine();
                         break;
 
@@ -89,67 +89,58 @@ namespace Algorithim_Practice
                                     file2[file2.Count - 1].Add(Arrays[i]);
                                 }
                             }
-                            for (int i = 0; i < file2.Count; i++)
-                            {
-                                bool isascending = true;
-                                bool isdescending = true;
-                                bool isvalid = true;
-                                for (int j = 0; j < file2[i].Count - i; j++)
-                                {
-                                    int right = 0;
-                                    int left = 0;
-                                    if (int.TryParse(file2[i][j], out left))
-                                    {
-                                        Console.WriteLine("Invalid");
-                                        isvalid = false;
-                                        break;
-                                    }
-                                    if (int.TryParse(file2[i][j], out right))
-                                    {
-                                        Console.WriteLine("Invalid");
-                                        isvalid = false;
-                                        break;
-                                    }
-
-
-                                    if (left < right)
-                                    {
-                                        isdescending = false;
-
-                                    }
-                                    if (left > right)
-                                    {
-                                        isascending = false;
-
-                                    }
-                                }
-
-                                if (isvalid == true)
-                                {
-                                    if (isascending == false && isdescending == false)
-                                    {
-                                        Console.WriteLine("Random");
-                                    }
-                                    else if (isdescending == true)
-                                    {
-                                        Console.WriteLine("is descending");
-                                    }
-                                    else if (isascending == true)
-                                    {
-                                        Console.WriteLine("is ascending");
-                                    }
-                                }
-
-
-                            }
-                            Console.ReadLine();
-
-                            break;
                         }
+                        for (int i = 0; i < file2.Count; i++)
+                        {
+                            bool isascending = true;
+                            bool isdescending = true;
+                            bool isvalid = true;
+                            for (int j = 0; j < file2[i].Count - 1; j++)
+                            {
+                                int right = 0;
+                                int left = 0;
+                                if (!int.TryParse(file2[i][j], out left))
+                                {
+                                    Console.WriteLine("Invalid");
+                                    isvalid = false;
+                                    break;
+                                }
+                                if (!int.TryParse(file2[i][j + 1], out right))
+                                {
+                                    Console.WriteLine("Invalid");
+                                    isvalid = false;
+                                    break;
+                                }
+                                if (left < right)
+                                {
+                                    isdescending = false;
+                                }
+                                if (left > right)
+                                {
+                                    isascending = false;
+                                }
+                            }
+                            if (isvalid == true)
+                            {
+                                if (isascending == false && isdescending == false)
+                                {
+                                    Console.WriteLine("Random");
+                                }
+                                else if (isdescending == true)
+                                {
+                                    Console.WriteLine("is descending");
+                                }
+                                else if (isascending == true)
+                                {
+                                    Console.WriteLine("is ascending");
+                                }
+                            }
 
+                        }
+                        Console.ReadLine();
+                        break;
                     case 3:
                         string text4 = AppDomain.CurrentDomain.BaseDirectory + @"Prob04.in.txt";
-
                         List<string> file3 = new List<string>();
                         using (StreamReader sr = new StreamReader(text4))
                         {
@@ -160,30 +151,23 @@ namespace Algorithim_Practice
                             }
                         }
                         int temp = 0;
-                        int tt5l = 0;
+                        int total = 0;
                         for (int i = 0; i < file3.Count; i++)
                         {
-
                             temp = int.Parse(file3[i]);
-                            tt5l = temp;
+                            total = temp;
                             temp -= 1;
-
                             while (temp > 0)
                             {
-                                tt5l = (tt5l * temp);
+                                total = (total * temp);
                                 temp--;
                             }
-                            Console.WriteLine(tt5l);
+                            Console.WriteLine(total);
                         }
-                        Console.ReadLine();
-
                         break;
-
-
                 }
+            } while (answer != 4);
 
-            }
         }
     }
 }
-
